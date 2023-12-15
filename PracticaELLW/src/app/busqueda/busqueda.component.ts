cfb2aac2f2c3e03362312bf46a44b15f2f43f52d
DidacEstrada <didac.estrada@estudiants.urv.cat>
2023-12-15 11:29:45 +0100

    classe-15

---
+++ PracticaELLW/src/app/busqueda/busqueda.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-busqueda',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './busqueda.component.html',
+  styleUrl: './busqueda.component.css'
+})
+export class BusquedaComponent {
+
+}
--- PracticaELLW/src/app/capsalera/capsalera.component.ts
+++ PracticaELLW/src/app/capsalera/capsalera.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavcapsaleraComponent } from '../navcapsalera/navcapsalera.component';
+import { BusquedaComponent } from '../busqueda/busqueda.component';
 import { LogoComponent } from '../logo/logo.component';
 
 @Component({
   selector: 'app-capsalera',
   standalone: true,
-  imports: [CommonModule, NavcapsaleraComponent, LogoComponent],
+  imports: [CommonModule, NavcapsaleraComponent, LogoComponent, BusquedaComponent],
   templateUrl: './capsalera.component.html',
   styleUrl: './capsalera.component.css'
 })
