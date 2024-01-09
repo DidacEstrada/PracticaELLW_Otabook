3889928380a15980aaa8e8b982eb9bb19366c440
DidacEstrada <didac.estrada@estudiants.urv.cat>
2024-01-09 20:31:38 +0100

    Plantilla manga

--- PracticaELLW/src/app/app.routes.ts
+++ PracticaELLW/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { MangaComponent } from './manga/manga.component';
+import { TopfiveComponent } from './topfive/topfive.component';
 
 
 export const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', component: TopfiveComponent },
+  {path: ':titulo', component: MangaComponent}
 ];
--- PracticaELLW/src/app/manga/manga.component.ts
+++ PracticaELLW/src/app/manga/manga.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { CapsaleraComponent } from '../capsalera/capsalera.component';
 
 @Component({
   selector: 'app-manga',
   standalone: true,
-  imports: [CommonModule, CapsaleraComponent],
+  imports: [CommonModule, CapsaleraComponent, RouterOutlet],
   templateUrl: './manga.component.html',
   styleUrl: './manga.component.css'
 })
-export class MangaComponent {
+export class MangaComponent implements OnInit{
+
+    titulo: string | undefined;
+    constructor(private route: ActivatedRoute) { }
+
+    ngOnInit(): void {
+      this.route.params.subscribe(params =>{
+        this.titulo = params['titulo'];
+      });
+
+    }
+
 
 }
