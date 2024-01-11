6f38ecb6c92cbfc3f25a75178bc0c806ceaeb057
adria <adrianzu14@gmail.com>
2024-01-11 23:46:52 +0100

    lista mangas

--- PracticaELLW/src/app/app.routes.ts
+++ PracticaELLW/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { MangaComponent } from './manga/manga.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { ListaMangasComponent } from './lista-mangas/lista-mangas.component';
 
 
 export const routes: Routes = [
   {path: '', component: PrincipalComponent},
-  {path: ':titulo', component: MangaComponent}
+  {path: ':titulo', component: MangaComponent},
+  {path: 'listar/Mangas', component: ListaMangasComponent},
+  {path: 'busqueda/:buscado', component: ListaMangasComponent}
 ];
